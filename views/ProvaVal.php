bc69a2c38376da1edf54f9b583e2286664ab1b76
Vanesa Bunjaku <vanesa.bunjaku@outlook.com>
2022-03-09 21:42:14 +0100

    VanesaErion

--- views/ProvaVal.php
+++ views/ProvaVal.php
@@ -1,9 +1,28 @@
 <?php
     $connect=mysqli_connect("localhost","root","","projekti") or die("Connection Failed");
     if(!empty($_POST['log']))
-    {
+    {   
         $username=$_POST['username'];
         $password=$_POST['password'];
+        include_once 'users.php';
+
+           $i=0;
+           foreach($users as $user){
+               $i++;
+               if($username == $user['username'] && $password == $user['password']){
+                   echo $username;
+                   echo $password;
+
+                   session_start();
+                   $_SESSION['username'] = $username;
+                   $_SESSION['password'] = $password;
+                   $_SESSION['role'] = $user['role'];
+                  // setcookie("username","$username",time()+(30*60));
+                  // setcookie("username","$username",time()-(60*60));
+                   header("location:Admin.php");
+                   exit();
+               }
+            }
         $query="select * from user where username='$username' and password='$password'";
         $result=mysqli_query($connect,$query);
         $count=mysqli_num_rows($result);
---
+++ views/users.php
@@ -0,0 +1,21 @@
+<?php 
+$user1 = [
+  "name"=>"Erion",
+  "surname" => "Maxhuni",
+  "email"=>"em52342@ubt-uni.net",
+  "username"=>"erionmax",
+  "password"=>"Maxhuni221",
+  "role"=>"admin"
+];
+$user2 = [
+    "name"=>"Vanesa",
+    "surname" => "Bunjaku",
+    "email"=>"Vb48564@ubt-uni.net",
+    "username"=>"Vanesab",
+    "password"=>"Bunjaku220",
+    "role"=>"admin"
+  ];
+
+  $users = [$user1,$user2];
+
+?>
